12df637d8a14376e7fdf1efa224fb144e79a5402
ttseric <ttseric@hotmail.com>
2013-11-07 22:41:43 +0800

    Commit

---
+++ XeniaWedding.Core/Data/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XeniaWedding.Core.Data
+{
+    public interface IRepository<T> where T : class
+    {
+        T GetById(object id);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        IQueryable<T> Table { get; }
+    }
+}
