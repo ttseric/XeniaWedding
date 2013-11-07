5f20f80bed892471a3a81198f288230df65988c7
ttseric <ttseric@hotmail.com>
2013-11-07 22:15:05 +0800

    Commit

---
+++ XeniaWedding.Core/SeatTable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XeniaWedding.Core
+{
+    public class SeatTable
+    {
+        private List<Guest> _guests;
+
+        public int Capacity { get; set; }
+
+        public string Name { get; set; }
+
+        public int TableNumber { get; set; }
+
+        public string Remark { get; set; }
+
+        public List<Guest> Guests
+        {
+            get { return _guests ?? (_guests = new List<Guest>()); }
+        }
+
+
+    }
+}
